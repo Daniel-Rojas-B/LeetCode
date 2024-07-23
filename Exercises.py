92f74f9c9bf0cc196021eb17c78eb377e4721a3d
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-22 19:25:09 -0700

    V07222024 Commit

---
+++ Exercises.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        for i in range(len(nums)):
+            ans.append(nums[i])
+        return ans
+
+solution = Solution()
+print(solution.getConcatenation([1,2,1]))
