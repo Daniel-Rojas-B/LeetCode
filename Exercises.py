2c633e5a621e6ad7fc70df9841d1fbcbb2a08eb0
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-22 22:43:37 -0700

    V07222024 10:43PM

--- Exercises.py
+++ Exercises.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    #1929. Concatenation of Array 
     def getConcatenation(self, nums):
         """
         :type nums: List[int]
@@ -10,6 +11,17 @@ class Solution(object):
         for i in range(len(nums)):
             ans.append(nums[i])
         return ans
+    #1920. Build Array from Permutation
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        for i in range(len(nums)):
+            ans.append(nums[nums[i]])
+        return ans
 
 solution = Solution()
 print(solution.getConcatenation([1,2,1]))
+print(solution.buildArray([0,2,1,5,3,4]))
