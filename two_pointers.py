68e5c9875894763dfb2198957a1d21eafd139e22
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-26 14:55:54 -0700

    Add two pointer exercise

--- two_pointers.py
+++ two_pointers.py
@@ -22,16 +22,28 @@ class Solution(object):
         :type ch: str
         :rtype: str
         """
-        
-        n=len(word)
-        for i in range(n):
+        word_length=len(word)
+        for i in range(word_length):
             if word[i]==ch:
-                return word[i::-1]+word[i+1:n]
-        return word
+                return word[i::-1]+word[i+1:word_length]
+        return False
+    
+    def minimumAverage(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: float
+        """
+        n=len(nums)        
+        nums.sort()
+        min_avg=(nums[0]+nums[-1])/2
+        for i in range(1,n//2):
+            avg=(nums[i]+nums[n-1-i])/2
+            if avg<min_avg:
+                min_avg=avg
+        return min_avg
 
 solution = Solution()
 print(solution.countPairs([-6,2,5,-2,-7,-1,3],-2))
 print(solution.reversePrefix('abcdefd','d'))
+print(solution.minimumAverage([7,8,3,4,15,13,4,1]))
 
-
-        
