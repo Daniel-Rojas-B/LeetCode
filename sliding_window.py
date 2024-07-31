436649af01bc606879a529c243b25a5c4af1ce44
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-07-30 22:22:31 -0700

    Add sliding window exercise

--- sliding_window.py
+++ sliding_window.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution(object):
     # 1876. Substrings of Size Three with Distinct Characters
     def countGoodSubstrings(self, s):
@@ -11,6 +13,37 @@ class Solution(object):
                 counter += 1
 
         return counter
+    # 219. Contains Duplicate II
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        hset={}
+        for i in range(len(nums)):
+            if nums[i] in hset and abs(i-hset[nums[i]]<=k):
+                return True
+            hset[nums[i]]=i
+        return False
+    # 594. Longest Harmonious Subsequence
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        tmp = Counter(nums)
+        print(tmp)
+        keys = tmp.keys()
+        print(keys2)
+        max = 0
+        for num in keys:
+            if num - 1 in keys:
+                if tmp[num - 1] + tmp[num] > max:
+                    max = tmp[num - 1] + tmp[num]
+        return max
 
 solution = Solution()
 print(solution.countGoodSubstrings("aababcabc"))
+print(solution.containsNearbyDuplicate([1,2,3,1,2,3],2))
+print(solution.findLHS([1,3,2,2,5,2,3,7]))
