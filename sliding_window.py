0d3914553d30eefa1a0b5e817df422511a13f72f
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-08-05 22:26:51 -0700

    Add sliding window exercise

--- sliding_window.py
+++ sliding_window.py
@@ -35,15 +35,39 @@ class Solution(object):
         tmp = Counter(nums)
         print(tmp)
         keys = tmp.keys()
-        print(keys2)
+        print(keys)
         max = 0
         for num in keys:
             if num - 1 in keys:
                 if tmp[num - 1] + tmp[num] > max:
                     max = tmp[num - 1] + tmp[num]
         return max
+    # 3206. Alternating Groups I
+    
+    def numberOfAlternatingGroups(self, colors):
+        """
+        :type colors: List[int]
+        :rtype: int
+        """
+        n = len(colors)
+        if n < 3:
+            return 0
 
+        ans = 0
+        for i in range(n - 2):
+            if colors[i] == colors[i + 2] and colors[i] != colors[i + 1]:
+                ans += 1
+        
+        if colors[0] == colors[n - 2] and colors[0] != colors[n - 1]:
+            ans += 1
+        
+        if colors[0] != colors[1] and colors[1] == colors[n - 1]:
+            ans += 1
+        
+        return ans
+    
 solution = Solution()
 print(solution.countGoodSubstrings("aababcabc"))
 print(solution.containsNearbyDuplicate([1,2,3,1,2,3],2))
 print(solution.findLHS([1,3,2,2,5,2,3,7]))
+print(solution.numberOfAlternatingGroups([1,1,1]))
