dea4f5974db3e1d648c457c348bf49c8f5f124d4
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-08-12 19:24:24 -0700

    Add Sliding Window Exercise

--- sliding_window.py
+++ sliding_window.py
@@ -111,7 +111,31 @@ class Solution(object):
                 if arr[ord(s[j]) - ord('a')] == 3:
                     break
                 maxLen = max(maxLen, j - i + 1)
-        return maxLen    
+        return maxLen
+
+    # 1763. Longest Nice Substring
+    def longestNiceSubstring(self, s: str) -> str:
+        
+        # If length of string is less than 2, there is no way we can get a nice substring out of it
+        if len(s) < 2 : return ""
+        
+        # A set of all characters in the given string
+        ulSet = set(s)
+        
+        for i,c in enumerate(s):
+            # If character is lowercase and the set does not have its uppercase variation or vice versa
+            # It means that we need to not consider this character at all and so, find the longest substring till this character and after this character
+            # Repeat this till we reach the end
+            if c.swapcase() not in ulSet:
+                s1 = self.longestNiceSubstring(s[0:i])
+                s2 = self.longestNiceSubstring(s[i+1:])
+                
+                return s2 if len(s2) > len(s1) else s1
+        
+        
+        # If the above recursive calls don't happen that means our string has each character in lowercase and uppercase already so we can return it
+        return s
+
 
 solution = Solution()
 print(solution.countGoodSubstrings("aababcabc"))
@@ -120,3 +144,4 @@ print(solution.findLHS([1,3,2,2,5,2,3,7]))
 print(solution.numberOfAlternatingGroups([1,1,1]))
 print(solution.decrypt([2,4,9,3],-2))
 print(solution.maximumLengthSubstring("bcbbbcba"))
+print(solution.longestNiceSubstring("YazaAay"))
