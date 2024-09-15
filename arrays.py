9cdfbcbc2a1fa9b2006d23f1bc05a0c8a6242c56
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-09-14 21:03:46 -0700

    Added arrays exercise

--- arrays.py
+++ arrays.py
@@ -77,7 +77,23 @@ class Solution(object):
                 return mid
         return l
 
-
+    # 66. Plus One
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        # Traverse the list from the last element to the first
+        for i in range(len(digits)-1, -1, -1):
+            # If the current digit is 9, set it to 0
+            if digits[i] == 9:
+                digits[i] = 0
+            else:
+                # If the current digit is not 9, increment it by 1 and return the list
+                digits[i] = digits[i] + 1
+                return digits
+        # If all digits are 9, prepend 1 to the list
+        return [1] + digits
     
 solution = Solution()
 print(solution.getConcatenation([1,2,1]))
@@ -87,3 +103,4 @@ print(solution.runningSum([1,2,3,4,5]))
 print(solution.removeDuplicates([2,1,1]))
 print(solution.removeElement([3,2,2,3],2))
 print(solution.searchInsert([1,3,5,6],7))
+print(solution.plusOne([1,2,3]))
