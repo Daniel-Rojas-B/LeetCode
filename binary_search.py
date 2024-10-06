93cf778d38b65f741dcab1c0b0673920a8eeebb8
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-10-06 12:51:39 -0700

    Added binary search exercise

--- binary_search.py
+++ binary_search.py
@@ -30,7 +30,30 @@ class Solution(object):
                 else: min_of_right = min(nums1[i], nums2[j])
 
                 return (max_of_left + min_of_right) / 2.0
-  
+
+    # 33. Search in Rotated Sorted Array
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
        
 solution = Solution()
 print(solution.findMedianSortedArrays([1,2], [3,4]))
+print(solution.search([4,5,6,7,0,1,2], 0))
