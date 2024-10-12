a4b81d48e7fd45c3f8f768a717cad0897cfe6e7d
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-10-12 12:37:19 -0700

    Added binary search exercise

--- binary_search.py
+++ binary_search.py
@@ -90,8 +90,32 @@ class Solution(object):
                 return mid
             
         return right
+    # 74. Search a 2D Matrix
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows,columns=len(matrix),len(matrix[0])
+        top,bottom=0,rows-1
+        while top<=bottom:
+            row=(top+bottom)//2
+            if target<matrix[row][0]:
+                bottom=row-1
+            elif target>matrix[row][-1]:
+                top=row+1
+            else:
+                break
+        row=(top+bottom)//2
+        l,r=0,columns-1
+        while l<=r:
+            mid=(l+r)//2
+            if target<matrix[row][mid]:
+                r=mid-1
+            elif target>matrix[row][mid]:
+                l=mid+1
+            else:
+                return True
+        return False
 solution = Solution()
 print(solution.findMedianSortedArrays([1,2], [3,4]))
 print(solution.search([4,5,6,7,0,1,2], 0))
 print(solution.searchRange([5,7,7,8,8,10], 8))
 print(solution.mySqrt(64))
+print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
