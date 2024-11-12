ac596217909cecfd0bf06afd6a0805fc28846200
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-11-11 19:30:10 -0800

    Added array exercise

--- arrays.py
+++ arrays.py
@@ -189,7 +189,36 @@ class Solution(object):
         result.extend(groups.values())
 
         return result
-       
+    
+    # 36. Valid Sudoku
+    def isValidSudoku(self, board):
+        return (self.is_row_valid(board) and
+            self.is_col_valid(board) and
+            self.is_square_valid(board))
+
+    def is_row_valid(self, board):
+        for row in board:
+            if not self.is_unit_valid(row):
+                return False
+        return True
+
+    def is_col_valid(self, board):
+        for col in zip(*board):
+            if not self.is_unit_valid(col):
+                return False
+        return True
+        
+    def is_square_valid(self, board):
+        for i in (0, 3, 6):
+            for j in (0, 3, 6):
+                square = [board[x][y] for x in range(i, i + 3) for y in range(j, j + 3)]
+                if not self.is_unit_valid(square):
+                    return False
+        return True
+        
+    def is_unit_valid(self, unit):
+        unit = [i for i in unit if i != '.']
+        return len(set(unit)) == len(unit)
 solution = Solution()
 print(solution.getConcatenation([1,2,1]))
 print(solution.buildArray([0,2,1,5,3,4]))
@@ -203,3 +232,13 @@ print(solution.twoSum([3,2,4],6))
 print(solution.maxArea([1,8,6,2,5,4,8,3,7]))
 print(solution.threeSum([0,0,0]))
 print(solution.groupAnagrams([""]))
+print(solution.isValidSudoku(board = 
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]))
