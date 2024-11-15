a73b22a9232f11483bf7a1227a98af944d7acf52
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-11-14 17:36:52 -0800

    Added arrays exercise

--- arrays.py
+++ arrays.py
@@ -239,8 +239,28 @@ class Solution(object):
                             dp[i].append(temp)
         return dp[target] 
 
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        candidates.sort()
+        self.backtrack(result, [], candidates, target, 0)
+        return result
+    
+    def backtrack(self, result: List[List[int]], current: List[int], candidates: List[int], remaining: int, start: int):
+        if remaining < 0:
+            return
+        elif remaining == 0:
+            result.append(list(current))
+        else:
+            for i in range(start, len(candidates)):
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                current.append(candidates[i])
+                self.backtrack(result, current, candidates, remaining - candidates[i], i + 1)
+                current.pop()
+
 solution = Solution()
     
+
 
 print(solution.getConcatenation([1,2,1]))
 print(solution.buildArray([0,2,1,5,3,4]))
@@ -265,3 +285,4 @@ print(solution.isValidSudoku(board =
 ,[".",".",".","4","1","9",".",".","5"]
 ,[".",".",".",".","8",".",".","7","9"]]))
 print(solution.combinationSum([2,3,6,7],7))
+print(solution.combinationSum2([10,1,2,7,6,1,5], 8))
