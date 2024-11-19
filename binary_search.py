e556414d030eea37d9c4b323c1b2c4f46a23fbdc
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2024-11-18 17:01:25 -0800

    Added binary_search exercise

--- binary_search.py
+++ binary_search.py
@@ -152,6 +152,27 @@ class Solution(object):
                 r = m
 
         return nums[l]
+    
+    # 162. Find Peak Element
+    def findPeakElement(self, nums):
+        n = len(nums)
+        if n == 1:
+            return 0
+        if nums[n-1] > nums[n-2]:
+            return n-1
+        if nums[0] > nums[1]:
+            return 0
+
+        low, high = 0, n-1
+        while low <= high:
+            mid = (low+high) >> 1
+            if nums[mid] > nums[mid-1] and nums[mid] > nums[mid+1]:
+                return mid
+            elif nums[mid] < nums[mid+1]:
+                low = mid + 1
+            else:
+                high = mid -1
+        return -1
 
 solution = Solution()
 print(solution.findMedianSortedArrays([1,2], [3,4]))
@@ -161,3 +182,4 @@ print(solution.mySqrt(64))
 print(solution.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],3))
 print(solution.search_two([2,5,6,0,0,1,2],0))
 print(solution.findMin([11,13,15,17]))
+print(solution.findPeakElement([1,2,1,3,5,6,4]))
