af021a1c65c78e93c2c5f52a737d6bfdcbbb34ab
Daniel R <152226403+Daniel-Rojas-B@users.noreply.github.com>
2025-01-07 21:29:48 -0800

    Added math exercise

--- math_.py
+++ math_.py
@@ -210,6 +210,24 @@ class Solution(object):
         # If all digits are 9, prepend 1 to the list
         return [1] + digits
 
+    def addBinary(self, a: str, b: str) -> str:
+        s = []
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+
+        while i >= 0 or j >= 0 or carry:
+            if i >= 0:
+                carry += int(a[i])
+                i -= 1
+            if j >= 0:
+                carry += int(b[j])
+                j -= 1
+            s.append(str(carry % 2))
+            carry //= 2
+
+        return ''.join(reversed(s))
+
 def linked_list_to_list(head):
     """Convert a linked list back to a Python list."""
     result = []
@@ -243,3 +261,4 @@ print(solution.myPow(2,10))
 print(solution.getPermutation(3,3))
 print(solution.uniquePaths(3,7))
 print(solution.plusOne([4,3,2,1]))
+print(solution.addBinary("11","1"))
